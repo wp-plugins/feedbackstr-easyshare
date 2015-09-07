6b44c2549845878842859adfd9bf93d4ced9a2d5
Spectos <Spectos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-07 11:06:29 +0000

    Bugfix - hided Checkboxes are visible now.
    git-svn-id: https://plugins.svn.wordpress.org/feedbackstr-easyshare/tags/1.0.0@1239681 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dashboard/footer.php
@@ -0,0 +1,20 @@
+</div>
+<div class="fdb-wrap d-hidden">
+	<div class="fdb-float-r fdb-w-50">
+		<a class="account" target="_blank"  href="https://www.feedbackstr.com/users/register"><?php _e('Request an account', 'fdbeasyshare') ;?></a>
+		<a class="cockpit" target="_blank"  href="https://feedbackstr.com"><?php _e('Login / Cockpit', 'fdbeasyshare') ;?></a>
+	</div>
+</div>
+<div class="fdb-wrap">
+	<div class="fdb-footer-l">
+		<?php _e('Feedbackstr is a product of <a target="_blank" href="http://spectos.com">Spectos</a> <br /><span style="color:#888;">Thank you for using Feedbackstr Easyshare V ###</span>','fdbeasyshare') ;?>
+	</div>
+	<div style="float:right; width:180px; padding: 22px 25px 20px 15px; text-align:right;">
+		<?php echo '<a href="https://www.facebook.com/pages/Feedbackstr-Deutschland/464798223658473?fref=ts" target="_blank"><img class="facebook"  src="' . plugins_url( 'images/f-facebook.png', __FILE__ ) . '" ></a>'; ?>
+		<?php echo '<a href="https://twitter.com/FeedbackstrDE" target="_blank"><img class="facebook"  src="' . plugins_url( 'images/f-twitter.png', __FILE__ ) . '" ></a>'; ?>
+		<?php echo '<a href="https://plus.google.com/+Feedbackstr/posts" target="_blank"><img class="facebook"  src="' . plugins_url( 'images/f-googleplus.png', __FILE__ ) . '" ></a>'; ?>
+		<?php echo '<a href="http://www.feedbackstr.com/de/feed/" target="_blank"><img class="facebook"  src="' . plugins_url( 'images/f-rss.png', __FILE__ ) . '" ></a>'; ?>
+	</div>
+</div>
+
+
---
+++ dashboard/header.php
@@ -0,0 +1,12 @@
+<div class="fdb-wrap">
+	<div class="fdb-easy-tab-topline">
+		<div class="fdb-float-l fdb-w-50">
+			<?php
+				echo '<img class="fdb-logo"  src="' . plugins_url( 'images/logo.png', __FILE__ ) . '" > ';
+			?>
+		</div>
+		<div class="fdb-float-r fdb-w-50 m-hidden" style="text-align:right;">
+			<a class="account" target="_blank"  href="https://www.feedbackstr.com/users/register"><?php echo _e('Request an account', 'fdbeasyshare') ;?></a>
+<a class="cockpit" target="_blank"  href="https://feedbackstr.com/users/login"><?php echo _e('Login / Cockpit', 'fdbeasyshare') ;?></a>
+		</div>
+	</div>
